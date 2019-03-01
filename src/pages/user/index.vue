b2581968c3c0c9e4fd2eed9c43a79b7aca1e9586
wangyess <936886321@qq.com>
2019-03-01 17:27:18 +0800

    增加选择图片拍照功能

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -4,6 +4,7 @@
     <div class="notice-box">
       <van-notice-bar text="有位非常漂亮的女同事，有天起晚了没有时间化妆便急忙冲到公司。结果那天她被记旷工了……吃惊" />
     </div>
+
     <!-- login -->
     <div class="login-box">
       <div class="login-img">
@@ -30,13 +31,69 @@
     <div class="items-box">
       <div class="item-card">
         <van-cell-group>
-        <van-cell title="收货地址" icon="location-o" is-link url="../../pages/adress/main" link-type="navigateTo" />
-        <van-cell title="联系客服" icon="audio" is-link />
-        <van-cell title="关于我们" icon="friends-o" is-link />
-        <van-cell title="版本信息" icon="new-o" is-link  url="../../pages/version/main" link-type="navigateTo"/>
+          <van-cell
+            title="收货地址"
+            icon="location-o"
+            is-link
+            url="../../pages/adress/main"
+            link-type="navigateTo"
+          />
+          <van-cell
+            title="联系客服"
+            icon="audio"
+            is-link
+          />
+          <van-cell
+            title="关于我们"
+            icon="friends-o"
+            is-link
+          />
+          <van-cell
+            title="版本信息"
+            icon="new-o"
+            is-link
+            url="../../pages/version/main"
+            link-type="navigateTo"
+          />
+          <van-cell
+            icon="video-o"
+            title="来张图片"
+            is-link
+            @click="chooseItem"
+          />
         </van-cell-group>
       </div>
     </div>
+
+    <van-popup
+      :show="show"
+      position="bottom"
+      :overlay="false"
+      bind:close="onClose"
+      custom-class="botom-style"
+    >
+      <div>
+        <van-button
+          custom-class="btn-style"
+          type="default"
+          @click="makePhoto"
+        >拍照</van-button>
+      </div>
+      <div>
+        <van-button
+          custom-class="btn-style"
+          type="default"
+          @click="takePhoto"
+        >从相册中选择</van-button>
+      </div>
+      <div>
+        <van-button
+          custom-class="btn-style"
+          type="default"
+          @click="cancelChoose"
+        >取消</van-button>
+      </div>
+    </van-popup>
   </div>
 </template>
 
@@ -45,16 +102,54 @@ export default {
   data () {
     return {
       user: {},
-      login: false
+      login: false,
+      show: false
     }
   },
   onShow () {
     this.get_user_data()
   },
-  mounted () {
-
-  },
   methods: {
+    getLimit () {
+      wx.getSetting({
+        success (res) {
+          wx.authorize({
+            scope: 'scope.writePhotosAlbum',
+            success () {
+            // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+            // wx.startRecord()
+            }
+          })
+        }
+      })
+    },
+    takePhoto () {
+      wx.chooseImage({
+        count: 1,
+        sizeType: ['original', 'compressed'],
+        sourceType: ['album'],
+        success (res) {
+          console.log(res.tempFilePaths)
+          // tempFilePath可以作为img标签的src属性显示图片
+        }
+      })
+    },
+    makePhoto () {
+      wx.chooseImage({
+        count: 1,
+        sizeType: ['original', 'compressed'],
+        sourceType: ['camera'],
+        success (res) {
+          console.log(res.tempFilePaths)
+        }
+      })
+    },
+    chooseItem () {
+      this.show = true
+    },
+    cancelChoose () {
+      this.show = false
+    },
     routerTo () {
       this.$router.push('/pages/authorization/main')
     },
@@ -87,7 +182,7 @@ export default {
 <style>
 .big-box {
   width: 750rpx;
-
+  margin-bottom: 200rpx;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -129,5 +224,16 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+}
+.btn-style {
+  width: 750rpx;
+}
+.botom-style {
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
